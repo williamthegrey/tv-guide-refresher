e75d4509671df4f0b7a6e805bd071d9b913eccaf
William Lu <wy.william.lu@gmail.com>
2020-12-09 22:26:16 +0800

    Trigger refreshing at a certain time of a day; Add a config file; Implement config changes; Add an icon showing int the Settings/Manage/Plugins UI

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -5,9 +5,12 @@ from Framework.api import ThreadKit
 from Framework.api.logkit import LogKit
 from Framework.api.networkkit import HTTPKit
 from Framework.api.parsekit import XMLKit
+from Framework.api.runtimekit import PrefsKit
 
 global Log
 Log = Log  # type: LogKit
+global Prefs
+Prefs = Prefs  # type: PrefsKit
 global HTTP
 HTTP = HTTP  # type: HTTPKit
 global XML
@@ -16,27 +19,49 @@ global Thread
 Thread = Thread  # type: ThreadKit
 
 base_url = 'http://127.0.0.1:32400'
-sleep_seconds = 5
-interval_seconds = 15
+refresh_time = ''
+job = None
+
+
+# noinspection PyUnresolvedReferences
+@handler('/video/tv_guide_refresher', 'TV Guide Refresher')
+def main_menu():
+    pass
 
 
 # noinspection PyPep8Naming
 def Start():
+    Log.Info('Create scheduler thread')
     # noinspection PyTypeChecker
-    Thread.Create(main)
+    Thread.Create(scheduler_thread)
+    schedule_job()
 
 
-def main():
-    Log.Info('Sleep for %d seconds' % sleep_seconds)
-    time.sleep(sleep_seconds)
-
-    schedule.every(interval_seconds).seconds.do(refresh_dvrs)
+def scheduler_thread():
     while True:
         schedule.run_pending()
         time.sleep(1)
 
 
+def schedule_job():
+    global refresh_time
+    new_refresh_time = Prefs['refresh_time']
+    if refresh_time == new_refresh_time:
+        return
+    refresh_time = new_refresh_time
+    Log.Info('Set refresh time to: ' + refresh_time)
+
+    global job
+    if job is not None:
+        Log.Info('Cancel the existing job')
+        schedule.cancel_job(job)
+        job = None
+    Log.Info('Schedule the job at refresh time')
+    job = schedule.every().day.at(refresh_time).do(refresh_dvrs)
+
+
 def refresh_dvrs():
+    Log.Info('Running the scheduled job')
     dvrs = get_dvrs()
     dvr_count = 0 if dvrs is None else len(dvrs)
     Log.Info('Found %d DVRs' % dvr_count)
@@ -58,3 +83,8 @@ def refresh_dvr(dvr):
     key = dvr.get('key')
     Log.Info('Refresh program guide for DVR %s' % key)
     HTTP.Request(base_url + '/livetv/dvrs/' + key + '/reloadGuide', immediate=True, method='POST')
+
+
+# noinspection PyPep8Naming
+def ValidatePrefs():
+    schedule_job()
